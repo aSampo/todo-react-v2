0ffe53ce38c75ea36860110144724b04b262ad2a
Andres Sampo <andressampo@gmail.com>
2021-07-02 18:36:39 -0300

    display list of task

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import React from 'react';
-import { ChakraProvider, Heading, Grid, theme } from '@chakra-ui/react';
+import {
+  ChakraProvider,
+  Heading,
+  Grid,
+  Text,
+  Checkbox,
+  Divider,
+  Stack,
+  theme,
+  Box,
+} from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import TaskInput from './components/TaskInput';
 
 function App() {
-  const [listOfTask, setTask] = React.useState([]);
+  const [listOfTask, setTasks] = React.useState([]);
 
   function handleSubmit(event) {
     event.preventDefault();
-    setTask([...listOfTask, event.target.elements.taskInput.value]);
+    setTasks([
+      ...listOfTask,
+      generateTask(event.target.elements.taskInput.value),
+    ]);
     event.target.elements.taskInput.value = '';
+  }
+
+  function onCheckChange(id, event) {
+    let index = listOfTask.findIndex(e => e.id === id);
+    let items = [...listOfTask];
+    let item = { ...items[index] };
+    item.checked = event.target.checked;
+    items[index] = item;
+    setTasks(items);
+  }
+
+  function generateTask(taskDescription) {
+    return {
+      id: Math.floor(Math.random() * Date.now()),
+      description: taskDescription,
+      checked: false,
+    };
   }
 
   return (
@@ -19,7 +49,30 @@ function App() {
         <Heading m="auto" size="lg">
           To-Do App 💙
         </Heading>
-        <TaskInput handleSubmit={handleSubmit}/>
+        <TaskInput handleSubmit={handleSubmit} />
+        {listOfTask
+          .filter(e => !e.checked)
+          .map(item => (
+            <Stack m="auto" direction="row" key={item.id}>
+              <Text fontSize="15px">{item.description}</Text>
+              <Checkbox
+                onChange={e => onCheckChange(item.id, e)}
+                isChecked={item.checked}
+              />
+            </Stack>
+          ))}
+        {listOfTask.filter(e => e.checked).length ? <Divider m="auto" /> : null}
+        {listOfTask
+          .filter(e => e.checked)
+          .map(item => (
+            <Stack m="auto" direction="row" key={item.id}>
+              <Text fontSize="15px" as="s">{item.description}</Text>
+              <Checkbox
+                onChange={e => onCheckChange(item.id, e)}
+                isChecked={item.checked}
+              />
+            </Stack>
+          ))}
       </Grid>
     </ChakraProvider>
   );
